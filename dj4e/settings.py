aa1fe120cf95fa960a230a435fc207ce26e1777d
jbuly4 <ibuly4@gmail.com>
2023-12-29 23:20:30 +0200

    prepared for task

--- dj4e/settings.py
+++ dj4e/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'dj4e.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
