5dc8d6286a0a77344724e3a03d6d89a6d1b59249
jbuly4 <ibuly4@gmail.com>
2023-12-30 15:16:25 +0200

    started. need to understand how context is formed

--- dj4e/urls.py
+++ dj4e/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('home.urls')),  # Change to ads.urls
+    path('', include('ads.urls')),  # Change to ads.urls
     path('admin/', admin.site.urls),  # Keep
     path('accounts/', include('django.contrib.auth.urls')),  # Keep
     re_path(r'^oauth/', include('social_django.urls', namespace='social')),  # Keep
